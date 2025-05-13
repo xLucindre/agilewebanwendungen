fa88004c7dbf6c6dac2a88db0d7133c8f4fe6ceb
lasershottt <laserknaller@gmail.com>
2025-05-13 13:19:01 +0200

    made simple login

--- blackdesert/frontend/views.py
+++ blackdesert/frontend/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return render(request, "test.html", {"props": "Hello world"})
+
+def register(request):
+    return render(request, "register.html")
+
+def login(request):
+    return render(request, "login.html")
